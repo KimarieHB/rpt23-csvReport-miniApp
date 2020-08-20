bea594471d4fa7ed69bafa288427fc07d38d4278
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-08-20 13:02:24 -0700

    Change/rename files, create input form for client

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3031;
 
-app.use(express.static('public'));
+app.use(express.static('client'));
 
 app.listen(port, () => {
   console.log(`Listening at localhost ${port}`);
