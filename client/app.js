0b9bc599bf4c1208a59cd2f4c7d5de6dcc8c9496
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-08-20 20:27:57 -0700

    Successfully receive input form data from client
    add example data files

--- client/app.js
+++ client/app.js
@@ -1,3 +1,3 @@
-const submit = () => {
-  
-}
+//<form enctype='multipart/form-data'> for files
+//add multiple <input type='file'> for multiple file upload
+//  mdn sending and retrieving form data
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3031;
+const logger = require('morgan');
+const parser = require('body-parser');
+const port = process.env.PORT || 3000;;
+
+app.use(logger('dev'));
+app.use(parser.urlencoded( {extended: false} ));
 
 app.use(express.static('client'));
 
@@ -8,6 +14,13 @@ app.listen(port, () => {
   console.log(`Listening at localhost ${port}`);
 })
 
+// Test
 app.get('/test', (req, res) => {
   res.send('Server is serving!');
+})
+
+app.post('/json_input', (req, res) => {
+  console.log(req.body);
+  console.log(req.headers);
+  res.send('Recieved data');
 })
