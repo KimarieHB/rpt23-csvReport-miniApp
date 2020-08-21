a677f1bb1023e748a0437be36396c0a7d9d40c6d
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-08-21 11:31:32 -0700

    Begin processing response data into csv

--- server.js
+++ server.js
@@ -20,21 +20,20 @@ app.get('/test', (req, res) => {
 })
 
 app.post('/json_input', (req, res) => {
-  let csvObj = {
-    tableHeaders: null,
-    tableRows: []
-  };
-  // data is string, fool!
-  let data = req.body['json-input'];
-  let newData = JSON.stringify(data);
-  newData = parser.json(newData);
-  console.log(newData);
-  
+  let data = JSON.parse(req.body['json-input']);
+  toCSVFormatter(data);
+  console.log(Object.values(data));
   console.log(req.body['json-input']);
+  
+  
+  
   // res.send back the newly formatted info
-  // page shows csv table and input form (ready for new input)
-  // use res.write for this?
-
   // does this go to / or a new endpoint?
   res.send('Recieved data');
 })
+
+const toCSVFormatter = (object) => {
+  let header = Object.keys(object).toString();
+  console.log(header);
+  
+}
